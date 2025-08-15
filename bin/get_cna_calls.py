9ffde305f7516e03d9a647cb8b25cc53a2ed1063
Shivani Guturu <shguturu@gmail.com>
2025-08-14 20:58:50 -0400

    keep all clinical and research cna calls, add tag for rescued and fold change filter

--- bin/get_cna_calls.py
+++ bin/get_cna_calls.py
@@ -16,42 +16,35 @@ def get_all_copy_number_calls(patient_json, research_access_cna_template, clinic
     research_copy_number_calls = get_research_access_copy_number_calls(patient_data, cmo_id, research_access_cna_template, research_access_copy_number_p_value_filter)
     research_copy_number_calls['patient_id'] = combined_id
 
-    all_copy_number_calls = merge_and_rescue_calls(clinical_copy_number_calls, research_copy_number_calls, access_copy_number_gene_list)
-
+    classified_research_access_calls = rescue_research_access_calls(clinical_copy_number_calls, research_copy_number_calls, access_copy_number_gene_list)
+    
+    all_copy_number_calls = pd.concat([clinical_copy_number_calls, classified_research_access_calls], ignore_index=True)
     all_copy_number_calls["cmo_patient_id"] = cmo_id
     all_copy_number_calls["dmp_patient_id"] = dmp_id
-    all_copy_number_calls = all_copy_number_calls[["sample_id", "patient_id", "cmo_patient_id", "dmp_patient_id", "hugo_symbol", "site", "fold_change", "p_val", "CNA_plasma", "CNA_tumor"]]
+    all_copy_number_calls = all_copy_number_calls[["sample_id", "patient_id", "cmo_patient_id", "dmp_patient_id", "hugo_symbol", "site", "fold_change", "p_val", "p_val_filter", "fc_filter"]]
 
     return save_to_csv(all_copy_number_calls, combined_id, "CNA")
 
-def merge_and_rescue_calls(clinical_copy_number_calls, research_copy_number_calls, access_copy_number_gene_list):
+def rescue_research_access_calls(clinical_copy_number_calls, research_copy_number_calls, access_copy_number_gene_list):
 
-    if not clinical_copy_number_calls.empty:
-        clinical_subset = clinical_copy_number_calls[['hugo_symbol', 'patient_id', 'CNA_tumor']]
-        all_copy_number_calls = research_copy_number_calls.merge(clinical_subset, how='left', on=["hugo_symbol", "patient_id"])
-    else: 
-        all_copy_number_calls = research_copy_number_calls
-        all_copy_number_calls['CNA_tumor'] = np.nan
+    clinical_set = set(zip(clinical_copy_number_calls['hugo_symbol'], clinical_copy_number_calls['patient_id']))
 
-    all_copy_number_calls['CNA_plasma'] = all_copy_number_calls.apply(assign_cna_category, axis=1)
-    all_copy_number_calls = all_copy_number_calls[all_copy_number_calls['CNA_plasma'] != "NA"]
-    all_copy_number_calls = all_copy_number_calls.drop_duplicates(subset=['cmo_patient_id', 'sample_id', 'hugo_symbol', 'site'])
+    def fc_filter(row):
+        fc = float(row.fold_change)
+        in_access_gene_list = row.hugo_symbol in access_copy_number_gene_list
+        in_clinical_list = (row.hugo_symbol, row.patient_id) in clinical_set
 
-    return all_copy_number_calls
+        # Passes research thresholds
+        if (fc >= 1.5 or fc <= -1.5) and in_access_gene_list:
+            return "PASS"
+        # Rescued because it's present in clinical calls
+        elif (fc >= 1.2 or fc <= -1.2) and in_clinical_list:
+            return "RESCUED"
+        else:
+            return "FAIL"
 
-def assign_cna_category(row):
-
-    fc = float(row.fold_change)
-    if fc > 1.5 and row.hugo_symbol in access_copy_number_gene_list:
-        return 'AMP'
-    elif fc < -1.5 and row.hugo_symbol in access_copy_number_gene_list:
-        return 'HOMDEL'
-    elif fc > 1.2 and pd.notna(row['CNA_tumor']):
-        return 'AMP'
-    elif fc < 1.2 and pd.notna(row['CNA_tumor']):
-        return 'HOMDEL'
-    else:
-        return "NA"
+    research_copy_number_calls['fc_filter'] = research_copy_number_calls.apply(fc_filter, axis=1)
+    return research_copy_number_calls
     
 def get_all_clinical_copy_number_calls(dmp_id, clinical_cna_file):
     
@@ -71,11 +64,10 @@ def get_all_clinical_copy_number_calls(dmp_id, clinical_cna_file):
                             clinical_cna_calls.append({"dmp_patient_id": dmp_id, "sample_id": sample_col, "hugo_symbol": hugo_symbol, "fold_change": fc})
  
     if not clinical_cna_calls:
-        clinical_cna_calls_df = pd.DataFrame(columns=["dmp_patient_id", "sample_id", "hugo_symbol", "fold_change", "CNA_tumor"])
+        clinical_cna_calls_df = pd.DataFrame(columns=["dmp_patient_id", "sample_id", "hugo_symbol", "fold_change"])
     else:
         clinical_cna_calls_df = pd.DataFrame(clinical_cna_calls)
         clinical_cna_calls_df["fold_change"] = pd.to_numeric(clinical_cna_calls_df["fold_change"])
-        clinical_cna_calls_df["CNA_tumor"] = np.where(clinical_cna_calls_df["fold_change"] > 0, "AMP", "HOMDEL")
     
     return clinical_cna_calls_df
 
@@ -101,11 +93,16 @@ def get_research_access_copy_number_calls(patient_data, cmo_id, research_access_
                         research_access_cna_calls.append({"cmo_patient_id": cmo_id, "sample_id": sample_id, "hugo_symbol": hugo, "site": site, "fold_change": fc, "p_val": p_val})
 
     if not research_access_cna_calls:
-        research_access_cna_calls_df = pd.DataFrame(columns=["cmo_patient_id", "sample_id", "hugo_symbol", "site", "fold_change", "p_val", "CNA_tumor"])
+        research_access_cna_calls_df = pd.DataFrame(columns=["cmo_patient_id", "sample_id", "hugo_symbol", "site", "fold_change", "p_val", "p_val_filter"])
     else:
         research_access_cna_calls_df = pd.DataFrame(research_access_cna_calls)
         if research_access_copy_number_p_value_filter:
             research_access_cna_calls_df = research_access_cna_calls_df[research_access_cna_calls_df["p_val"].astype(float) < float(research_access_copy_number_p_value_filter) ]
+            threshold = float(research_access_copy_number_p_value_filter)
+            research_access_cna_calls_df["p_val"] = research_access_cna_calls_df["p_val"].astype(float)
+            research_access_cna_calls_df["p_val_filter"] = np.where(research_access_cna_calls_df["p_val"] > threshold, "FAIL", "PASS")
+        else:
+            research_access_cna_calls_df["p_val_filter"] = ""
         
     return research_access_cna_calls_df
 
